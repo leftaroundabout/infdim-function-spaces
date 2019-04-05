e8e70d9aa5c27fdc23a9c7b531a5b4a6a2245e51
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2019-04-05 16:24:12 +0200

    Initial commit.

---
+++ Math/Function/FiniteElement/PWLinear.hs
@@ -0,0 +1,19 @@
+-- |
+-- Module      : Math.Function.FiniteElement.PWLinear
+-- Copyright   : (c) Justus Sagemüller 2019
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) jsagemue $ uni-koeln.de
+-- Stability   : experimental
+-- Portability : portable
+-- 
+
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE FlexibleContexts       #-}
+{-# LANGUAGE TypeFamilies           #-}
+{-# LANGUAGE LambdaCase             #-}
+{-# LANGUAGE ConstraintKinds        #-}
+
+module Math.Function.FiniteElement.PWLinear
+        () where
+
