77e02180d37e7cd106f4d86be34be1a0ceadcb63
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2019-04-12 13:30:53 +0200

    Correct domain type for the functions.

--- Math/Function/FiniteElement/PWConst.hs
+++ Math/Function/FiniteElement/PWConst.hs
@@ -39,24 +39,28 @@ data Haar₀ y
                (Haar₀ y) -- ^ Left half of the function domain
                (Haar₀ y) -- ^ Right half, i.e. [0.5 .. 1[.
 
-data instance Haar ℝ y = Haar_ℝ
-    { pwconst_ℝ_offset :: !y
-    , pwconst_ℝ_variation :: Haar₀ (Diff y) }
+data instance Haar D¹ y = Haar_D¹
+    { pwconst_D¹_offset :: !y
+    , pwconst_D¹_variation :: Haar₀ (Diff y) }
 
-evalHaar_ℝ :: (AffineSpace y, VectorSpace (Diff y))
-              => Haar ℝ y -> ℝ -> y
-evalHaar_ℝ (Haar_ℝ offs varis) x = offs .+^ evalVari varis x
+evalHaar_D¹ :: (AffineSpace y, VectorSpace (Diff y))
+              => Haar D¹ y -> D¹ -> y
+evalHaar_D¹ (Haar_D¹ offs varis) x = offs .+^ evalVari varis x
  where evalVari HaarZero _ = zeroV
-       evalVari (Haar₀ δlr lh rh) x
-        | x<0.5      = evalVari lh (x*2) ^-^ δlr
-        | otherwise  = evalVari lh (x*2 - 1) ^+^ δlr
+       evalVari (Haar₀ δlr lh rh) (D¹ x)
+        | x<0        = evalVari lh (D¹ $ x*2 + 1) ^-^ δlr
+        | otherwise  = evalVari lh (D¹ $ x*2 - 1) ^+^ δlr
 
 newtype PowerOfTwo = PowerOfTwo { binaryExponent :: Int } deriving (Eq, Ord, Show)
 
-homsampleHaar_ℝ :: (AffineSpace y, Diff y ~ y, VectorSpace y, Fractional (Scalar y))
-            => PowerOfTwo -> (ℝ -> y) -> Haar ℝ y
-homsampleHaar_ℝ (PowerOfTwo 0) f = Haar_ℝ (f 0.5) HaarZero
-homsampleHaar_ℝ (PowerOfTwo i) f
-   = case homsampleHaar_ℝ (PowerOfTwo $ i-1) <$> [f . (/2), f . (/2).(+1)] of
-       [Haar_ℝ y₀l sfl, Haar_ℝ y₀r sfr]
-        -> Haar_ℝ ((y₀l^+^y₀r)^/2) $ Haar₀ ((y₀r^-^y₀l)^/2) sfl sfr
+homsampleHaar_D¹ :: (AffineSpace y, Diff y ~ y, VectorSpace y, Fractional (Scalar y))
+            => PowerOfTwo -> (D¹ -> y) -> Haar D¹ y
+homsampleHaar_D¹ (PowerOfTwo 0) f = Haar_D¹ (f $ D¹ 0) HaarZero
+homsampleHaar_D¹ (PowerOfTwo i) f
+   = case homsampleHaar_D¹ (PowerOfTwo $ i-1) <$> [f . leftHalf, f . rightHalf] of
+       [Haar_D¹ y₀l sfl, Haar_D¹ y₀r sfr]
+        -> Haar_D¹ ((y₀l^+^y₀r)^/2) $ Haar₀ ((y₀r^-^y₀l)^/2) sfl sfr
+
+leftHalf, rightHalf :: D¹ -> D¹
+leftHalf (D¹ x) = D¹ $ (x-1)/2
+rightHalf (D¹ x) = D¹ $ (x+1)/2
