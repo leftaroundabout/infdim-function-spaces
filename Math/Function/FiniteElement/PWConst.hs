2dfcd4f9e94797d00b4c7fe9f2ab484ad4fb862e
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2019-04-13 12:44:31 +0200

    Clearer terminology for PowerOfTwo type.

--- Math/Function/FiniteElement/PWConst.hs
+++ Math/Function/FiniteElement/PWConst.hs
@@ -51,13 +51,15 @@ evalHaar_D¹ (Haar_D¹ offs varis) x = offs .+^ evalVari varis x
         | x<0        = evalVari lh (D¹ $ x*2 + 1) ^-^ δlr
         | otherwise  = evalVari rh (D¹ $ x*2 - 1) ^+^ δlr
 
-newtype PowerOfTwo = PowerOfTwo { binaryExponent :: Int } deriving (Eq, Ord, Show)
+newtype PowerOfTwo = TwoToThe { binaryExponent :: Int } deriving (Eq, Ord, Show)
+getPowerOfTwo :: PowerOfTwo -> Integer
+getPowerOfTwo (TwoToThe ex) = 2^ex
 
 homsampleHaar_D¹ :: (AffineSpace y, Diff y ~ y, VectorSpace y, Fractional (Scalar y))
             => PowerOfTwo -> (D¹ -> y) -> Haar D¹ y
-homsampleHaar_D¹ (PowerOfTwo 0) f = Haar_D¹ (f $ D¹ 0) HaarZero
-homsampleHaar_D¹ (PowerOfTwo i) f
-   = case homsampleHaar_D¹ (PowerOfTwo $ i-1) <$> [f . leftHalf, f . rightHalf] of
+homsampleHaar_D¹ (TwoToThe 0) f = Haar_D¹ (f $ D¹ 0) HaarZero
+homsampleHaar_D¹ (TwoToThe i) f
+   = case homsampleHaar_D¹ (TwoToThe $ i-1) <$> [f . leftHalf, f . rightHalf] of
        [Haar_D¹ y₀l sfl, Haar_D¹ y₀r sfr]
         -> Haar_D¹ ((y₀l^+^y₀r)^/2) $ Haar₀ ((y₀r^-^y₀l)^/2) sfl sfr
 
